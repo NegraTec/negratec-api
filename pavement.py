69a512a12eb213cee5fbdde01388ffc4d79aadbd
Roselma Mendes <roselma.mendes@gmail.com>
2016-04-05 13:50:53 -0300

    Adiciona o paver para automatizar algumas tarefas.

--- pavement.py
+++ pavement.py
@@ -1,8 +1,20 @@
 from paver.easy import task
 from paver.shell import sh
+from paver.tasks import needs
 
 
 @task
+@needs('migration')
 def test():
-
     sh('python ./manage.py test')
+
+
+@task
+@needs('migration')
+def server():
+    sh('python ./manage.py runserver')
+
+
+@task
+def migration():
+    sh('python ./manage.py makemigrations; python ./manage.py migrate')
